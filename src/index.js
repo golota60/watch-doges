99dab101131de31115d9ef9954f9de6c8dade433
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-10-11 21:18:54 +0200

    imp: remove unnecessary files and set up prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { hydrate, render } from "react-dom";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
 } else {
